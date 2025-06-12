0fd5de0e9eb7d86c8108a508edde1de2aa6d8ec1
Omansh <omanshawasthi@gmail.com>
2025-06-12 21:39:52 +0530

    signup authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+
+dotenv.config();
 
 const app=express();
+const PORT=process.env.PORT;
 
 app.use("/api/auth",authRoutes);
 
-app.listen(5001,()=>{
-    console.log("server running on port 5001");
+app.listen(PORT,()=>{
+    console.log("server running on port PORT:"+PORT);
+    connectDB();
 })
